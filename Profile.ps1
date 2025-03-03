4b763b9dfb071cbbd009a141a8335e4b058e4baf
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2025-03-03 11:29:00 +0100

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -2,4 +2,5 @@
 Set-PSReadlineKeyHandler -Key ctrl+d -Function DeleteCharOrExit
 
 # History show full history not session history
+Remove-Item Alias:history
 function history { Get-Content (Get-PSReadLineOption).HistorySavePath }
